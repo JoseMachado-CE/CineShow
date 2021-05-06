0c24d7a6e8fc3071f356a75880b8fc0406727fe2
JoseMachado-CE <jose@letsgetchecked.com>
2021-05-06 12:53:42 +0000

    Updated the script file adding localstorage function to it and appeding it to the movies id

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,5 +43,45 @@ function movieSelected(id) {
 }
 
 function getMovie() {
-    
+    let movieId = sessionStorage.getItem('movieId');
+
+    axios.get(base_omd + '&i=' + movieId)
+        .then((response) => {
+            console.log(response);
+            let movie = response.data;
+
+            let output = `
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well-2">
+            <h3>Movie Overview</h3>
+            ${movie.Plot}
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn butao">View IMDB</a>
+            <a href="index.html" class="btn butao">Go Back To Search</a>
+          </div>
+        </div>
+        
+      `;
+
+            $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
