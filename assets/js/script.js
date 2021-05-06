d3954a69b5dd5aebf0038689aaa98a01735d10a8
JoseMachado-CE <jose@letsgetchecked.com>
2021-05-06 11:25:01 +0000

    Updated the script file adding jquery document read for clicking search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 const base_omd = 'https://www.omdbapi.com/?apikey=9473d9c9';
 const api_key = '9473d9c9';
 
+$(document).ready(() => {
+    $('#buttonGen').on('click', (e) => {
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+});
