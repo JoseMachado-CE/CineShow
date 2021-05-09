551afa437c9270960e42735b499ca01adbaa61e8
JoseMachado-CE <jose@letsgetchecked.com>
2021-05-09 17:04:55 +0000

    Updated style file adding bg color the the box search movie class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 const base_omd = 'https://www.omdbapi.com/?apikey=9473d9c9';
 const api_key = '9473d9c9';
-
 $(document).ready(() => {
     $('#buttonGen').on('click', (e) => {
         let searchText = $('#searchText').val();
@@ -8,74 +7,55 @@ $(document).ready(() => {
         e.preventDefault();
     });
 });
-
 function getMovies(searchText) {
     $('.hide-bottom-cine').hide();
     axios.get(base_omd + '&s=' + searchText)
         .then((response) => {
-                console.log(response);
-                let movies = response.data.Search;
-                let output = '';
-                $.each(movies, (index, movie) => {
-                        if (movie.Title.length <= 19) {
-                            output += `
-                <div class="centered">
-                    <section class="cards">
-                        <article class="card">
-                            <p><img src="${movie.Poster}" alt="" /></p>
-                            <p>${movie.Title}</p>
-                            <p>${movie.imdbID}</p>
-                        </article><!-- /card-one -->
-                    </section>
+            console.log(response);
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie) => {
+                output += `
+          <div class="col-md-4 col-lg-3 box-search-long">
+                <div class="content-box-search">
+                    <div class="box-movies-search">
+                        <img src="${movie.Poster}" alt="" />
+                    </div>
+                    <div class="title-movie-search"
+                        <h5>${movie.Title}</h5>
+                    </div>
+                    <a onclick="movieSelected('${movie.imdbID}')" class="btn butao2" href="#">Movie Details</a>
                 </div>
-             `;
-                            /* } else {
-                    output += `
-               <div class="row col-md-4 text-center box">
-                    <div class="col-1">
-                        <div class="card ">
-                            <div class="card-header">
-                                <a onclick="movieSelected('${movie.imdbID}')" class="btn butao2" href="#">Movie Details</a>
-                            </div>
-                            <div class="card-bodys">
-                                <p class="card-title title-movie-h5">${movie.Title}</p>
-                            </div>
-                            
-                            <img src="${movie.Poster} alt="" />
-                        </div>
-                     </div>
-                </div>      
-             `;
-                }*/
+          </div>
+          
+        `;
+            });
+            $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
 
-                        };
+function getMovie() {
 
+ 
+    let movieId = sessionStorage.getItem('movieId');
 
-                        $('#movies').html(output);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
+    axios.get(base_omd + '&i=' + movieId)
+        .then((response) => {
+            console.log(response);
+            let movie = response.data;
 
-            function movieSelected(id) {
-                sessionStorage.setItem('movieId', id);
-                window.location = 'movie.html';
-                return false;
-            }
-
-            function getMovie() {
-                let movieId = sessionStorage.getItem('movieId');
-
-                axios.get(base_omd + '&i=' + movieId)
-                    .then((response) => {
-                        console.log(response);
-                        let movie = response.data;
-
-                        let output = `
+            let output = `
         <div class="row">
           <div class="col-md-4">
-            <img src="${movie.Poster}" class="thumbnail img-details">
+            <img src="${movie.Poster}" class="thumbnail">
           </div>
           <div class="col-md-8">
             <h2>${movie.Title}</h2>
@@ -101,9 +81,9 @@ function getMovies(searchText) {
         
       `;
 
-                        $('#movies').html(output);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
+            $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
