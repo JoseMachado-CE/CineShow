a5f46111c6a44bfdb0f94f7476ddd317af918e63
JoseMachado-CE <jose@letsgetchecked.com>
2021-05-09 18:04:06 +0000

    Updated the script file adding row and cols to the movie overview template literals

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,34 +53,40 @@ function getMovie() {
             let movie = response.data;
 
             let output = `
-        <div class="row">
-          <div class="col-sm-12 col-md col-lg-4">
-            <img src="${movie.Poster}" class="thumbnail">
-          </div>
-          <div class="col-sm-12 col-md col-lg-8">
-            <h2>${movie.Title}</h2>
-            <ul class="list-group">
-              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="well-2">
-            <h3>Movie Overview</h3>
-            ${movie.Plot}
-          </div>
-          <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn butao3">View IMDB</a>
-            <a href="index.html" class="btn butao3">Go Back To Search</a>
-        </div>
-        
-      `;
-
+                <div class="row">
+                    <div class="col-sm-12 col-md col-lg-4">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    <div class="col-sm-12 col-md col-lg-8">
+                        <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row movie-overview">
+                    <div class="col">
+                        <h3>Movie Overview</h3>
+                        ${movie.Plot}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn butao3">View IMDB</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a href="index.html" class="btn butao3">Go Back To Search</a>
+                    </div>
+                </div>
+            `;
             $('#movies').html(output);
         })
         .catch((err) => {
