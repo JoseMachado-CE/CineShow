57c64addca09240a726746f558c2c7541d5871c8
JoseMachado-CE <jose@letsgetchecked.com>
2021-05-09 18:44:48 +0000

    Updated the map js file adding  adding jquery ready function

--- assets/js/map.js
+++ assets/js/map.js
@@ -9,3 +9,36 @@ var marker0 = L.marker([53.3513, -6.2605])
 var marker1 = L.marker([53.3503, -6.2677])
 var marker2 = L.marker([53.2889, -6.2220])
 var marker3 = L.marker([53.2934, -6.1397])
+
+$(document).ready(function () {
+    $('#btn1').on('click', function (e) {
+        e.preventDefault();
+        layerGroup.clearLayers();
+        map.closePopup();
+        marker = marker0.addTo(layerGroup);
+        marker.bindPopup("Savoy Cinema").openPopup();
+        $('#display-here').html(`
+                     <div class="container">
+                        <div class="row featurette">
+                            <div class="col-md-7 low-cont">
+                        <h2 class="featurette-heading">Savoy Cinemas</h2>
+                        <figure>
+                            <blockquote class="blockquote">
+                                <p class="lead">The Savoy Cinema is the oldest operational cinema in Dublin, and it is the preferred cinema in Ireland for film premières.
+                                </p>
+                            </blockquote>
+                            <h5>Address</h5>
+                            <p class="lead">17 O'Connell Street Upper, North City, Dublin 1</p>
+                            <h5>Website</h5>
+                            <p class="lead">https://imccinemas.ie/</p>
+                            <a type="submit" class="btn butao2" href="location.html">Back to cinemas</a>
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-fluid mx-auto img-locatio-cine low-cont d-none d-md-block" alt="" src="savoy.jpg">
+                    </div>
+                    
+            </div>
+                `);
+    })
+});
