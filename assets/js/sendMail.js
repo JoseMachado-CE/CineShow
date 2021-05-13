8bf3acc8cbd9713649e46257b66aaca80e35383e
JoseMachado-CE <jose@letsgetchecked.com>
2021-05-13 21:41:21 +0000

    Updated the sendmail file adding a form validation

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -8,11 +8,31 @@ function sendMail(contactForm) {
 
     emailjs.send('service_nujzdlk', 'template_qonn19p', tempParams)
     .then(function(res) {
-        console.log('success', res.status);
+        if($('#fullNameId').val() == '') {
+            $('#name-response').text('You need to enter your name');
+            $('#fullNameId').focus();
+            return false;
+        }else if($('#exampleInputEmail1').val() == '') {
+            $('#email-response').text('You need to enter your email');
+            $('#exampleInputEmail1').focus();
+            return false;
+        }else if($('exampleFormControlTextarea1').val() == '') {
+            $('#text-response').text('Leave a mensage please');
+            $('#exampleFormControlTextarea1').focus();
+            return false;
+        }else {
+            console.log('success', res.status);
+            $('#text-response').html('<p>Your text has been sent</p>');
+            $('#name-response').hide();
+            $('#email-response').hide();
+            $('#text-response').hide();
+            return true;
+        }
+        
         
     })
-    document.getElementById("form").reset();
     
 }
+
 
 
