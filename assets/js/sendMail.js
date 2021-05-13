a816ed5ab943e327479bd8526de90055465ab171
JoseMachado-CE <jose@letsgetchecked.com>
2021-05-13 21:57:40 +0000

    Updated the style file adding colors to the form validation

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,38 +1,32 @@
-  
 function sendMail(contactForm) {
     var tempParams = {
-        from_name:document.getElementById('fullNameId').value,
-        from_email:document.getElementById('exampleInputEmail1').value,
-        message:document.getElementById('exampleFormControlTextarea1').value,
+        from_name:$('#fullNameId').val(),
+        from_email:$('#exampleInputEmail1').val(),
+        message:$('#exampleFormControlTextarea1').val(),
     };
 
     emailjs.send('service_nujzdlk', 'template_qonn19p', tempParams)
-    .then(function(res) {
-        if($('#fullNameId').val() == '') {
-            $('#name-response').text('You need to enter your name');
-            $('#fullNameId').focus();
-            return false;
-        }else if($('#exampleInputEmail1').val() == '') {
-            $('#email-response').text('You need to enter your email');
-            $('#exampleInputEmail1').focus();
-            return false;
-        }else if($('exampleFormControlTextarea1').val() == '') {
-            $('#text-response').text('Leave a mensage please');
-            $('#exampleFormControlTextarea1').focus();
-            return false;
-        }else {
-            console.log('success', res.status);
-            $('#text-response').html('<p>Your text has been sent</p>');
-            $('#name-response').hide();
-            $('#email-response').hide();
-            $('#text-response').hide();
-            return true;
-        }
-        
-        
-    })
-    
+        .then(function (res) {
+            if ($('#fullNameId').val() == '') {
+                $('#name-response').html('<p class="errMensage">You need to add your name</p>');
+                $('#fullNameId').focus();
+                return false;
+            } else if ($('#exampleInputEmail1').val() == '') {
+                $('#email-response').html('<p class="errMensage">You need to add your email</p>');
+                $('#exampleInputEmail1').focus();
+                return false;
+            } else if ($('exampleFormControlTextarea1').val() == '') {
+                $('#text-response1').html('<p class="errMensage">Your need to leave a mensage</p>');
+                $('#exampleFormControlTextarea1').focus();
+                return false;
+            } else {
+                console.log('success', res.status);
+                $('#text-response').html('<p class="sucsMensage">Your text has been sent</p>');
+                $('#name-response').hide();
+                $('#email-response').hide();
+                $('#text-response1').hide();
+                $('#form')[0].reset();
+                return true;
+            }
+        })
 }
-
-
-
